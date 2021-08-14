00c68b5b87c1b4cce2cea9d429df966e6a4132c1
irahel <isaac_rahel@live.com>
2021-08-14 11:11:03 -0300

    Start creation SimpleReport/generate method

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,34 @@
+import datetime
+class SimpleReport:
+    def __create_date(self, entry):
+        splited = entry.split('-')
+        return datetime.date(int(splited[0]), int(splited[1]), int(splited[2])) 
+
+    def generate(self, data):        
+        today = datetime.date.today()        
+        older_date = datetime.date.max
+        nearest_expiration = datetime.date.min
+        companys = {}
+        #print(older_date)
+        #print(nearest_expiration)
+        for report in data:
+            manufacturing_date = self.__create_date(report['data_de_fabricacao'])
+            expiration_date = self.__create_date(report['data_de_validade'])
+
+            if manufacturing_date < older_date:
+                older_date = manufacturing_date
+                #print(older_date)
+            
+            if (not(expiration_date > today)) and ((expiration_date - today) > (nearest_expiration - today)):
+                nearest_expiration = expiration_date
+                #print(nearest_expiration)
+            
+            if report['nome_da_empresa'] in companys:
+                companys[report['nome_da_empresa']] += 1
+            else:
+                companys[report['nome_da_empresa']] = 1
+                
+        formated_report = "Data de fabricação mais antiga: "+str(older_date) +"\n" \
+                            +"Data de validade mais próxima: " +str(nearest_expiration) + "\n" \
+                                +"Empresa com maior quantidade de produtos estocados: " +max(companys, key=companys.get)
+        return formated_report
