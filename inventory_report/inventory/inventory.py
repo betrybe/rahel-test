fe7901b4a67f7b572652fd7ccc60323757961bd0
irahel <isaac_rahel@live.com>
2021-08-14 14:56:08 -0300

    json inventory complete

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,19 +1,47 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 SIMPLE = "simples"
 COMPLETE = "completo"
 
 
 class Inventory:
+    def __csv_handler(file_path):
+        with open(file_path, newline='') as csv_file:
+            reader = csv.DictReader(csv_file)
+            return list(reader)
+    """
+    :author: Rahel
+    __csv_handler(file_path)
+        Returns a list of dict using csv entry
+    :param file_path: string
+    :return: list of dict
+    """
+
+    def __json_handler(file_path):
+        with open(file_path) as json_file:
+            reader = json.loads(json_file.read())
+            return list(reader)
+    """
+    :author: Rahel
+    __json_handler(file_path)
+        Returns a list of dict using json entry
+    :param file_path: string
+    :return: list of dict
+    """
+
     def import_data(path, type):
-        with open(path, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            if type == SIMPLE:
-                return SimpleReport.generate(list(reader))
-            elif type == COMPLETE:
-                return CompleteReport.generate(list(reader))
+        if ".csv" in path:
+            report = Inventory.__csv_handler(path)
+        elif ".json" in path:
+            report = Inventory.__json_handler(path)
+
+        if type == SIMPLE:
+            return SimpleReport.generate(report)
+        elif type == COMPLETE:
+            return CompleteReport.generate(report)
     """
     :author: Rahel
     import_data(path, type)
@@ -22,3 +50,5 @@ class Inventory:
     :param type: string
     :return: string
     """
+
+# print(Inventory.import_data("C:/Users/isaac/Desktop/trybe/rahel-test/inventory_report/data/inventory.csv", "completo"))
