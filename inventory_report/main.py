6ff4fdeb0cc494f55f1a893cbb2f23386c35845d
irahel <isaac_rahel@live.com>
2021-08-14 22:35:43 -0300

    start implement of main

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,35 @@
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+import sys
+
+SIMPLE = "simples"
+COMPLETE = "completo"
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        print("Verifique os argumentos", file=sys.stderr)
+    else:
+        print(sys.argv)
+        path = sys.argv[1]
+        type_report = sys.argv[2]
+
+        if ".csv" in path:
+            report = InventoryRefactor(CsvImporter)
+        elif ".json" in path:
+            report = InventoryRefactor(JsonImporter)
+        elif ".xml" in path:
+            report = InventoryRefactor(XmlImporter)
+        
+        report.import_data(path, type_report)
+
+        if type_report == SIMPLE:
+            print(SimpleReport.generate(report.data))
+        elif type_report == COMPLETE:
+            print(CompleteReport.generate(report.data))
+
+main()
